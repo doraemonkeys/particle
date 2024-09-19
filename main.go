1863a6757276ac76db1903421a0e216dd5d92dc7
Ricky <doraemonkey@qq.com>
2024-09-19 17:12:05 +0800

    remove unnecessary comment

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var (
 	host      = flag.String("host", "http://127.0.0.1:8384", "syncthing host")
 	user      = flag.String("user", "", "syncthing user")
 	pwdFile   = flag.String("pwdFile", "", "syncthing password file")
-	syncthing = flag.String("syncthing", "", "syncthing executable file") // optional，用于解析相对路径时使用
+	syncthing = flag.String("syncthing", "", "syncthing executable file")
 )
 
 var logger *logrus.Logger
